511f7383be270afa529ac6e267bc433f72db8b01
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-11-19 18:18:05 -0500

    Updated schema to reflect params outlined in the seed.js file

--- models/fitness.js
+++ models/fitness.js
@@ -3,31 +3,36 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const fitnessSchema = new Schema({
-    type: {
-        type: String,
-        trim:true,
-        required: "Enter the type of exercise"
+    day: {
+        type: Date,
     },
-    name: {
-        type: String,
-        trim: true,
-        required: "Enter the name of the exercise"
-    },
-    weight: {
-        type: Number
-    },
-    sets: {
-        type: Number
-    },
-    reps: {
-        type: Number
-    },
-    duration: {
-        type: Number
-    }, 
-    distance: {
-        type: Number
-    }
+    exercises: [
+        {type: {
+            type: String,
+            trim:true,
+            required: "Enter the type of exercise"
+        },
+        name: {
+            type: String,
+            trim: true,
+            required: "Enter the name of the exercise"
+        },
+        weight: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        duration: {
+            type: Number
+        }, 
+        distance: {
+            type: Number
+        }
+        }]
 });
 
 const Fitness = mongoose.model("Fitness", fitnessSchema);
