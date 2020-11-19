32972025b710d75d4d8015c874950522a1fec408
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-11-19 17:18:27 -0500

    Created models folder and fitness schema

---
+++ models/fitness.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const fitnessSchema = new Schema({
+    type: {
+        type: String,
+        trim:true,
+        required: "Enter the type of exercise"
+    },
+    name: {
+        type: String,
+        trim: true,
+        required: "Enter the name of the exercise"
+    },
+    weight: {
+        type: Number
+    },
+    sets: {
+        type: Number
+    },
+    reps: {
+        type: Number
+    },
+    duration: {
+        type: Number
+    }, 
+    distance: {
+        type: Number
+    }
+});
+
+const Fitness = mongoose.model("Fitness", fitnessSchema);
+
+module.exports = Fitness;
