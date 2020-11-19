089b914b3cfae10a9fa7661f89024bc97f3ba388
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-11-19 18:33:28 -0500

    Added api routes

---
+++ routes/api-routes.js
@@ -0,0 +1,25 @@
+// Dependencies
+const router = require("express").Router();
+const Exercise = require("../models/fitness.js");
+
+router.post("/api/workouts", ({body}, res) => {
+    Exercise.create(body)
+    .then(dbExercise => {
+        res.json(dbExercise);
+    })
+    .catch(err => {
+        res.status(400).json(err);
+    });
+});
+
+router.get("/api/workouts", (req, res) => {
+    Exercise.find({})
+      .sort({ date: -1 })
+      .then(dbExercise => {
+        res.json(dbExercise);
+      })
+      .catch(err => {
+        res.status(400).json(err);
+      });
+  });
+  
