3d5ad22e54555bf8e070462382a1b3e3b565a4e6
Andrew Elliott <andrewjc.elliott@gmail.com>
2020-11-20 22:05:42 -0500

    Updated route express structure based on mini project routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,8 +2,8 @@
 const router = require("express").Router();
 const Exercise = require("../models/fitness.js");
 
-router.post("/api/workouts", ({body}, res) => {
-    Exercise.create(body)
+router.post("/api/workouts", (req, res) => {
+    Exercise.create({})
     .then(dbExercise => {
         res.json(dbExercise);
     })
@@ -21,6 +21,25 @@ router.get("/api/workouts", (req, res) => {
       .catch(err => {
         res.status(400).json(err);
       });
+  });
+
+  router.put("/api/workouts/:id", ({ params }, res) => {   
+    Exercise.update(
+      {
+        _id: mongojs.ObjectId(params.id)
+      },
+      {
+        $set: {
+          exercises: body
+        }
+      }
+    )
+    .then(dbExercise => {
+      res.json(dbExercise);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
   });
   
   module.exports = router;
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,18 +1,17 @@
 // Dependencies
-const express = require("express");
+const router = require("express").Router();
 const path = require("path");
-const app = express();
 
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname,"../public/index.html"));
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
-app.get("/exercise", (req, res) => {
-    res.sendFile(path.join(__dirname,"../public/exercise.html"));
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
-app.get("/stats", (req, res) => {
-    res.sendFile(path.join(__dirname,"../public/stats.html"))
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
 });
 
-module.exports = app;
+module.exports = router;
